7e6c499b0c9d7fc5d8f014cede64c5c460667541
Prince-Automation <careers.prince.castelino@gmail.com>
2025-04-26 23:50:09 -0400

    Annotations TestNG

--- FrameworkPractice/src/framework/TestRunning.java
+++ FrameworkPractice/src/framework/TestRunning.java
@@ -1,9 +1,28 @@
 package framework;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestRunning {
 
+	@AfterTest
+	public void postRequisite() {
+		System.out.println("Execute this last");
+	}
+	
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("This is before Suite");
+	}
+	
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("Execute this last Suite");
+	}
+	
 	@Test
 	public void Demo() {
 		System.out.println("hello");
@@ -12,6 +31,11 @@ public class TestRunning {
 	@Test
 	public void Demo2() {
 		System.out.println("Test Case 2");
+	}
+	
+	@BeforeTest
+	public void preRequisite() {
+		System.out.println("Execute this first");
 	}
 
 }
